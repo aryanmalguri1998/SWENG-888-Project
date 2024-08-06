9f8ecf37843743a50865542f6568e0ae033dcf9a
Aryan Malguri <abm7104@psu.edu>
2024-08-06 14:39:39 -0700

    Update MainActivity.java

--- app/src/main/java/com/example/sweng888/MainActivity.java
+++ app/src/main/java/com/example/sweng888/MainActivity.java
@@ -14,6 +14,7 @@ import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
+    // Declare a DrawerLayout instance to manage the navigation drawer
     private DrawerLayout drawerLayout;
 
     @Override
@@ -21,28 +22,34 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Set up the toolbar
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        // Initialize the DrawerLayout and NavigationView
         drawerLayout = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
+        // Set up ActionBarDrawerToggle to open/close the navigation drawer
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav,
                 R.string.close_nav);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
 
+        // Display HomeFragment by default when the activity is first created
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
             navigationView.setCheckedItem(R.id.nav_home);
         }
     }
 
+    // Handle navigation item selection
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
 
+        // Switch fragments based on the selected item in the navigation drawer
         if (id == R.id.nav_home) {
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
         } else if (id == R.id.nav_settings) {
@@ -55,12 +62,15 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new LogoutFragment()).commit();
         }
 
+        // Close the navigation drawer after an item is selected
         drawerLayout.closeDrawer(GravityCompat.START);
         return true;
     }
 
+    // Handle the back button press
     @Override
     public void onBackPressed() {
+        // Close the navigation drawer if it is open
         if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
             drawerLayout.closeDrawer(GravityCompat.START);
         } else {
